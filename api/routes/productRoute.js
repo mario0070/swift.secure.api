e4298e6a88134d50fe73f6a4d3715ceece2ab42f
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-01-25 23:38:11 +0100

    product controller

--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -4,7 +4,7 @@ const productController = require("../controllers/productController")
 
 route.get("/", productController.getAllProduct)
 route.post("/create", productController.createproduct)
-route.get("/show", productController.show)
+route.post("/show", productController.show)
 route.post("/delete", productController.deleteProduct)
 route.get("/search-product", productController.searchProduct)
 route.post("/vendor-product", productController.getProductByOwner)
