6626e30f4cb922317ef5e2efbeb78f5117ddb274
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-02-05 14:41:14 +0100

    fix

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ function generateString(length) {
 
 app.get('/auth/google', (req, res) => {
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile email`;
-  res.redirect(url);
+  res.status(200).json({url})
+//   res.redirect(url);
 });
 
 app.get('/auth/google/callback', async (req, res) => {
